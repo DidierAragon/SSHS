e98d2e3e7aa0971a3e1150f13c3f69ff4d35a677
didier aragon <aragondidier19@gmail.com>
2025-07-05 15:43:31 -0500

    Sistema de Seguiminto  de Habitos Saludables

--- codigo.py
+++ codigo.py
@@ -50,15 +50,15 @@ while True:
                 break
             i += 1
         if dia_duplicado:
-            print("este dia ya esta registrado")
+            print("----este dia ya esta registrado----")
    
    # luego solicito los dato al usuario como el agua consumida,  los minutos de ejercicio, las horas de sueño y si se alimento bien
       
         else:
-            a = input("litros de agua consumido: ")
-            e = input("minutos de ejercicio: ")
-            s = input("horas de sueño: ")
-            c = input("alimentacion saludable? (si/no): ").lower()
+            a = input("-litros de agua consumido: ")
+            e = input("-minutos de ejercicio: ")
+            s = input("-horas de sueño: ")
+            c = input("-alimentacion saludable? (si/no): ").lower()
             error = False
    
     # convierto los datos que me dio el usuario a float e int
@@ -81,28 +81,28 @@ while True:
                     seguimiento.append((dia,agua, ejercicio, sueño, c))
                     print("registro exitoso")
             except:
-                print("datos incorrecto, ingresar datos correctos.")
+                print("\ndatos incorrecto, ingresar datos correctos.")
 
     elif opcion == "2":
   
     # creo un print donde le doy la bienvenida al historial, luego creo un contador vacio 
     #abro un while para contar los datos de la lista de seguimiento donde guardamos las atualizaciones que el #usuario escribe 
        
-        print("historial de habitos")
+        print("\n----historial de habitos----")
         i = 0
         while i < len(seguimiento):
    
     # creo una variable que toma los valores que conto la variable "i" y que muestra cada elemento de la lista #que el usuario ingreso luego muestro el historial en un print
    
             reg = seguimiento[i]
-            print(f"{reg[0]},\n agua= {(reg[1])},\n ejercicio= {(reg[2])} \nminutos sueño= {(reg[3])},\n alimentacion saludable= {(reg[4])}")
+            print(f"{reg[0]},\n-agua= {(reg[1])},\n-ejercicio= {(reg[2])} \n-minutos sueño= {(reg[3])},\n-alimentacion saludable= {(reg[4])}")
             i += 1
     elif opcion == "3":
 
     #creo un if que cuenta los elemento de la lista y si la lista esta vacia entonces muestro un mensaje que dice que no hay registro y por lo tanto no se puede calcular
         
         if len(seguimiento) == 0:
-            print("no hay registro para calcular")
+            print("\n----no hay registro para calcular----")
     # pero si en la lista hay elementos entoces creo 4 contadores con para cada elemento de la lista
         else:
             total_agua = 0
@@ -117,9 +117,55 @@ while True:
                 total_ejercicio += seguimiento[i][2]
                 total_sueño += seguimiento[i][3]
                 i += 1
+   
     #creo una variable de dias el cual cuenta los valores que hay dentro de la lista para luego dividirlo por #cada elemento y asi obtener el promedio por ultimo solo imprimo el promedio de cada area
+               
                 dias = len(seguimiento)
-            print("----promedio semanal------- ")
-            print(f"agua: {total_agua / dias} l")
-            print(f"ejercicio: {total_ejercicio / dias} M")
-            print(f"sueño: {total_agua / dias} H")
+            print("\n----promedio semanal----")
+            print(f"agua: {total_agua / dias:2f} litros")
+            print(f"ejercicio: {total_ejercicio / dias:2f} Minutos")
+            print(f"sueño: {total_sueño / dias:2f} Horas")
+    elif opcion == "4":
+   
+    # hago lo mismo que antes creo un if que cuenta los elemento de la lista y si la lista esta vacia entonces muestro un mensaje que dice que no hay registro y por lo tanto no se puede calcular
+        
+        if len(seguimiento) == 0:
+            print("\n----no hay registro para calcular----")
+   
+    #si la lista tiene elementos entoces creo un contador para cada habito y otro contador mas que recorre la lista
+       
+        else:
+            cumplir_agua = 0
+            cumplir_ejercicio = 0
+            cumplir_sueño = 0
+            cumplir_alimentacion = 0
+            i = 0
+   
+    #es while lo que hace es que el contador que recorre la lista va buscando si los elementos de la lista coincide con los parametros que que to le pido para que se cumple y si se cumple va sumando en los contadores de cada habito
+           
+            while i < len(seguimiento):
+                cumplir_agua  += seguimiento[i][1]  >= 2 
+                cumplir_ejercicio += seguimiento[i][2] >= 30
+                cumplir_sueño += seguimiento[i][3] >= 8
+                cumplir_alimentacion += seguimiento[i][4] == "si"
+                i += 1
+    
+    #la variable dias cuenta los dias que hay registrado en la lista y dentro de los print pongo directamente la formula para hallar el porcentaje de cumplimiento de cada habito
+                
+                dias = len(seguimiento)
+            print("\n----promedio de cumplimiento----")
+            print(f"-agua: {(cumplir_agua/dias)*100}%")
+            print(f"-ejercicio: {(cumplir_ejercicio/dias)*100}%")
+            print(f"-sueño: {(cumplir_sueño/dias)*100}%")
+            print(f"-alimentacion: {(cumplir_alimentacion/dias)*100}%")
+    
+    #luego creo un diccionario que reune los datos de cumplimiento y creo una  variable el cual busca el que tenga menor datos y le muestra un consejo al usuario para que mejore el habito que menos practico
+
+            porcentaje = {
+                "agua" : cumplir_agua,
+                "ejercicio" : cumplir_ejercicio,
+                "sueño" : cumplir_sueño,
+                "alimentacion" : cumplir_alimentacion
+            }
+            menor=min(porcentaje)
+            print(f"\nDeberias mejoras tu habito de: {menor} ya que lo practicas menos")
